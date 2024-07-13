3052decab742463ca495887b9827b62aeb29206a
maryannegathura <mariannegathura7@gmail.com>
2024-07-13 12:26:42 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Sophia Gathura';
+const welcomeMessage = 'Welcome to Flatbook, ${Sophia Gathura}!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, &{SOPHIA GATHURA.toUpperCase()}!';
+const firstInitial = currentUser[0];
+const shortGreeting = 'Welcome, ${S}!';
+module.exports = {currentUser, welcomeMessage, excitedWelcomeMessage, shortGreeting};
+
+
